55749717bac1f5a125024b0eb3ca355c701219b0
Prajwal H S <prajwalhs4india@gmail.com>
2026-02-02 14:28:50 +0530

    changes

--- src/components/Footer/BelowFooter/FooterCopy.jsx
+++ src/components/Footer/BelowFooter/FooterCopy.jsx
@@ -3,9 +3,10 @@ import "./../Footer.css"
 const FooterCopy = () => {
   return (
     <div className="footerCopy">
-      <p>© 2023 Tryseoservices.Com All Rights Reserved.</p>
+      <p>© 2023 Tryseoservices.Com All Rights Reserved. | <a href="/sitemap.xml">Sitemap</a></p>
     </div>
   );
 };
 
 export default FooterCopy;
+
